d7600087e114b916ae548157e1ca2c771ecc5244
Debitos_Aspire <Debitos_Aspire@DESKTOP-1OBMOAB>
2023-02-07 14:00:43 +0700

    Update

--- src/Tugas2_1.java
+++ src/Tugas2_1.java
@@ -1,12 +1,22 @@
 public class Tugas2_1 {
     public static void main(String[] args) {
-        System.out.println("1. " + kali(3, 3));
-        System.out.println("2. a(1) * b(9) = " + kali(1, 9));
-        System.out.println("3. " + kali(3, 3, 2));
-        System.out.println("4. " + kali(2.5, 7.0));
+        System.out.println("1. " + Kali.kali(3, 3));
+        System.out.println("2. a(1) * b(9) = " + Kali.kali(1, 9));
+        System.out.println("3. " + Kali.kali(3, 3, 2));
+        System.out.println("4. " + Kali.kali(2.5, 7.0));
     }
 }
 
 class Kali {
-    
+    static int kali(int a, int b) {
+        return a * b;
+    }
+
+    static double kali(double a, double b) {
+        return a * b;
+    }
+
+    static int kali(int a, int b, int c) {
+        return a * b * c;
+    }
 }
